3706f49a0eb57ee8f70684c653656c1170a0aeb1
SpiritualU <137093050+SpiritualU@users.noreply.github.com>
2023-07-13 17:42:20 -0500

    Update settings.js

--- theme/src/config/settings.js
+++ theme/src/config/settings.js
@@ -3,6 +3,6 @@ export default {
 	description: `SpiritualU is dedicated to help people worldwide in navigativing through the difficult and confusing journey by providing resources that create a roadmap for sucess`,
 	url: `https://odyssey-theme.littlesticks.dev`, // No trailing slash!
 	name: `SpiritualU`, // The short name of the business or brand name. Used for things like the copyright in the footer.
-  enableThemeSwitcher: true,
+  enableThemeSwitcher: false,
   showLittleSticksPlug: false, // Disable this if you want to remove the Little Sticks plug from the footer. (╯°□°)╯︵ ┻━┻
 };
